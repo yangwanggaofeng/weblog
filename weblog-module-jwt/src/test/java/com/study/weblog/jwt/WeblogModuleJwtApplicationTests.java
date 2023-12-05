dca5e9855c40a37c765f8b0c12016c489789cf36
zhangbb <zhangbb@shindata.com>
2023-12-05 13:15:51 +0800

    feat(weblog weblog-module-jwt）

---
+++ weblog-module-jwt/src/test/java/com/study/weblog/jwt/WeblogModuleJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.weblog.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeblogModuleJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
